3aa11fd2811cb4ff1dd29207ca0216955da10d38
Dawid Zawadzki <lamazlo21@gmail.com>
2020-11-09 14:45:52 +0100

    Implement LoadingPage page that it fetches data from pokeapi.co on init

--- lib/src/pages/loading_page.dart
+++ lib/src/pages/loading_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+// Utils
+import 'package:pokedex/src/utils/api/poke_api.dart';
+import 'package:pokedex/src/utils/models/pokemon.dart';
 
 class LoadingPage extends StatefulWidget {
   @override
@@ -6,6 +12,31 @@ class LoadingPage extends StatefulWidget {
 }
 
 class _LoadingPageState extends State<LoadingPage> {
+
+  void fetchAllPokemons() async {
+    List<Pokemon> pokemons = List<Pokemon>();
+    PokeApi api = PokeApi();
+    try {
+      Response pokesRes = await api.getAllPokemons();
+      Map pokemonsData = jsonDecode(pokesRes.body);
+      pokemonsData['results'].forEach((element) async {
+        String name = element['name'];
+        Response pokeRes = await api.getSpecificPokemonByName(name);
+        Map pokemonData = jsonDecode(pokeRes.body);
+        String imageUrl = pokemonData['sprites']['front_default'];
+        pokemons.add(Pokemon(name: name, imageUrl: imageUrl));
+      });
+    } catch(err) {
+      print('err');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAllPokemons();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
