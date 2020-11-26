07b091b19f94a07f5390d18ba522d8acc1e02cc4
Dawid Zawadzki <lamazlo21@gmail.com>
2020-11-27 00:07:07 +0100

    Merge push_notifications branch with master

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex/src/pages/loading_page.dart';
 import 'package:pokedex/src/pages/wiki_page.dart';
 import 'package:pokedex/src/pages/main_screen.dart';
 import 'package:pokedex/src/utils/api/parsing_service.dart';
+import 'package:pokedex/src/utils/api/notification_service.dart';
 
 class App extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    final push_notification_sevice = PushNotificationService();
+    push_notification_sevice.initialize();
+
     return MaterialApp(
       // Route indicated in initial route always has priority before '/' route
       initialRoute: '/',
